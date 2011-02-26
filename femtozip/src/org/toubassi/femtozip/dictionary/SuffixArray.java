f6bd76ed8bb1776f874ffe9242271ae67b1f7f1c
Garrick Toubassi <gtoubassi@yahoo.com>
2011-02-25 20:20:22 -0800

    Add apache 2.0 header

--- femtozip/src/org/toubassi/femtozip/dictionary/SuffixArray.java
+++ femtozip/src/org/toubassi/femtozip/dictionary/SuffixArray.java
@@ -1,3 +1,25 @@
+/**
+ *   Copyright 2011 Garrick Toubassi
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *   
+ *   
+ *   This file is a Java port of two files found at http://www.cs.dartmouth.edu/~doug/sarray/
+ *   
+ *   sarray.c: suffixarray generation, written by Sean Quinlan and Sean Doward
+ *   lcp.c: lcp computation implemented by Douglass McIlroy, method by Kasai et al.
+ *   See comments in sarray.c and lcp.c for more details.
+ */
 package org.toubassi.femtozip.dictionary;
 
 import java.io.PrintStream;
